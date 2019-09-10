6a8605728d78c345d5d5d0010f1ba37748900591
Drew Stone <drewstone329@gmail.com>
2019-09-11 01:01:37 +0300

    More

--- srml/staking/src/lib.rs
+++ srml/staking/src/lib.rs
@@ -1188,7 +1188,7 @@ impl<T: Trait> Module<T> {
 			for (v, points) in validators.iter().zip(rewards.rewards.into_iter()) {
 				if points != 0 {
 					let reward = multiply_by_rational(total_payout, points, total_points);
-					println!("Validator {:?}, Reward {:?}", v, reward);
+					// println!("Validator {:?}, Reward {:?}", v, reward);
 					total_imbalance.subsume(Self::reward_validator(v, reward));
 				}
 			}
